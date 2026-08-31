60f7108a3650b4b05787737fe372dc40972dedc6
alyssonale66-prog <alyssonale66@gmail.com>
2026-08-31 04:42:06 -0300

    Update MainActivity.java

--- android/app/src/main/java/com/fx/finance/MainActivity.java
+++ android/app/src/main/java/com/fx/finance/MainActivity.java
@@ -1,12 +1,16 @@
 package com.fx.finance;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // O bloqueio de print (FLAG_SECURE) agora é opcional e gerenciado via JS pelo usuário.
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        );
     }
 }
